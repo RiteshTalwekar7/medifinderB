36e83f62e8ddd814fba02052fec3c52c645581b6
Ritesh Talwekar <160117323+RiteshTalwekar7@users.noreply.github.com>
2025-04-01 12:25:02 +0530

    Update SuperAdminController.java
    Added API Endpoints and tested the api

--- medifinderB/src/main/java/com/medifinder/SuperAdmin/SuperAdminController.java
+++ medifinderB/src/main/java/com/medifinder/SuperAdmin/SuperAdminController.java
@@ -1,10 +1,26 @@
 package com.medifinder.SuperAdmin;
-
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/superAdmin")
 public class SuperAdminController {
-
+  @Autowired
+	private SuperAdminService superAdminService;
+	
+	@GetMapping(value = "/ViewAdmin")
+	public List<SuperAdmin>	getAllAdmins()
+	{
+		return superAdminService.getAllAdmins();
+	}
+  
+	 @PostMapping(value = "/AddNewAdmin")
+	 public void addStudent(@RequestBody SuperAdmin admin) {
+		 superAdminService.addAdmin(admin);
+	 }
 }
