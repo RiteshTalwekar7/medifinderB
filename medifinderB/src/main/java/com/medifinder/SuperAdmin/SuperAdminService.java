9b7daf978fb039710b66fdb9e39d289a63be8b10
Ritesh Talwekar <160117323+RiteshTalwekar7@users.noreply.github.com>
2025-04-01 12:29:23 +0530

    Super Admin Completed {Main functionality of adding and viewing all admins only}
    Added Business logics to service class for actual functionality of the Restful API endpoints.

--- medifinderB/src/main/java/com/medifinder/SuperAdmin/SuperAdminService.java
+++ medifinderB/src/main/java/com/medifinder/SuperAdmin/SuperAdminService.java
@@ -1,8 +1,21 @@
 package com.medifinder.SuperAdmin;
-
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SuperAdminService {
-
+  @Autowired
+	private SuperAdminRepository superAdminRepository;
+	
+	public List<SuperAdmin> getAllAdmins(){
+	    List<SuperAdmin> superadmin = new ArrayList<>();
+	    superAdminRepository.findAll()
+	    .forEach(superadmin::add);
+	    return superadmin;		
+	}
+	
+	public void addAdmin(SuperAdmin superAdmin) {
+		superAdminRepository.save(superAdmin);
+	}
 }
